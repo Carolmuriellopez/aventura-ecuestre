2d0f1f9fe08b2d6dbf32ef2b67eb1e80592396f4
Irene Mendoza <mendozagonzalez.irene@gmail.com>
2026-05-05 19:41:16 +0200

    fix: update ALLOWED_HOSTS for DigitalOcean deployment

--- aventura_ecuestre/settings.py
+++ aventura_ecuestre/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = [
-    'aventura-ecuestre.onrender.com',
+    'aventuraecuestre.irenemendoza.dev',
     'localhost', '127.0.0.1'
 ]
 
